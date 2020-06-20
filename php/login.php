565177b379223c2b454b5ff40250a2ded5ebc0c4
Andrey Sokolov <keremet@solaris.kirov.ru>
2020-06-20 22:32:23 +0300

    Исправлена ошибка отсутствия переменной

--- php/login.php
+++ php/login.php
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
+	$error_msg = "";
 	if( isset($_POST['login']) && isset($_POST['passwd'])  ) {
 		include('connect.php'); 
 		$stmt = $db->prepare(
